530ed7769618c16c5e4bc6813a57e6223024d83f
MCLEANS <jackagwaya@gmail.com>
2020-05-11 15:07:36 +0300

    Included i2s dependency headers

--- airrohr-firmware/SPH0645.h
+++ airrohr-firmware/SPH0645.h
@@ -1,6 +1,13 @@
 #ifndef _SPH0645_H
 #define _SPH0645_H
 
+extern "C" {
+#include "user_interface.h"
+#include "i2s_reg.h"
+#include "slc_register.h"
+#include "esp8266_peri.h"
+void rom_i2c_writeReg_Mask(int, int, int, int, int, int);
+}
 
 
 #endif //_SPH0645_H
