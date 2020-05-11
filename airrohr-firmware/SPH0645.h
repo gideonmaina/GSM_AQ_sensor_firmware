613eadf4f8c2551c876bd40fb7dae912b715c669
MCLEANS <jackagwaya@gmail.com>
2020-05-11 15:09:17 +0300

    definition of i2s pins, frequency and buffer details

--- airrohr-firmware/SPH0645.h
+++ airrohr-firmware/SPH0645.h
@@ -9,5 +9,17 @@ extern "C" {
 void rom_i2c_writeReg_Mask(int, int, int, int, int, int);
 }
 
+//#define DEBUG
+
+#define I2S_CLK_FREQ      160000000  // Hz
+#define I2S_24BIT         3     // I2S 24 bit half data
+#define I2S_LEFT          2     // I2S RX Left channel
+
+#define I2SI_DATA         12    // I2S data on GPIO12
+#define I2SI_BCK          13    // I2S clk on GPIO13
+#define I2SI_WS           14    // I2S select on GPIO14
+
+#define SLC_BUF_CNT       8     // Number of buffers in the I2S circular buffer
+#define SLC_BUF_LEN       64    // Length of one buffer, in 32-bit words.
 
 #endif //_SPH0645_H
