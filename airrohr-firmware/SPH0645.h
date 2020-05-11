0aa48e1a5009d25227498e78b63c8482e06bb0c5
MCLEANS <jackagwaya@gmail.com>
2020-05-11 15:12:11 +0300

    Macro to convert i2s data

--- airrohr-firmware/SPH0645.h
+++ airrohr-firmware/SPH0645.h
@@ -22,4 +22,14 @@ void rom_i2c_writeReg_Mask(int, int, int, int, int, int);
 #define SLC_BUF_CNT       8     // Number of buffers in the I2S circular buffer
 #define SLC_BUF_LEN       64    // Length of one buffer, in 32-bit words.
 
+/**
+ * Convert I2S data.
+ * Data is 18 bit signed, MSBit first, two's complement.
+ * Note: We can only send 31 cycles from ESP8266 so we only
+ * shift by 13 instead of 14.
+ * The 240200 is a magic calibration number I haven't figured
+ * out yet.
+ */
+#define convert(sample) (((int32_t)(sample) >> 13) - 240200)
+
 #endif //_SPH0645_H
