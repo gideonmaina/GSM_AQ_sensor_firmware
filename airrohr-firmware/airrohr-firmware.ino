3d1e0fddfbb9d58cfbc4ea06e23b5dd0077d5ab2
Warukira <kirah.gtheuri@gmail.com>
2020-08-07 12:17:45 +0300

    Clean up and formatting

--- airrohr-firmware/airrohr-firmware.ino
+++ airrohr-firmware/airrohr-firmware.ino
@@ -2594,7 +2594,7 @@ static unsigned long sendSD(const String &data, const int pin, const __FlashStri
 	}
 	else
 	{
-		switch_status_LEDs_off(LOGGER_LED,HIGH);	//	turn logger status led off
+		switch_status_LEDs_off(LOGGER_LED,HIGH);	// turn logger status led off
 		debug_outln_info("error logging data!!");
 	}
 
@@ -2803,10 +2803,10 @@ String fetchSensorDHTFromAtmega(){
 		}
 	}
 
-	toggle_status_LEDs(DHT_LED,LOW,HIGH,5000);	//	turn DHT status LED on for 5 seconds
+	toggle_status_LEDs(DHT_LED,LOW,HIGH,5000);	// turn DHT status LED on for 5 seconds
 
 	obtain_sendTime();
-	toggle_status_LEDs(RTC_LED,LOW,HIGH,5000);	//	turn RTC status LED on for 5 seconds
+	toggle_status_LEDs(RTC_LED,LOW,HIGH,5000);	// turn RTC status LED on for 5 seconds
 
 	debug_outln_info(FPSTR(DBG_TXT_SEP));
 	debug_outln_verbose(FPSTR(DBG_TXT_END_READING), FPSTR(SENSORS_DHT22));
@@ -3263,8 +3263,13 @@ String fetchSensorPMSFromAtmega(){
 		}
 
 		obtain_sendTime();
-		toggle_status_LEDs(PMS_LED,LOW,HIGH,5000);	//	turn PMS status LED on for 5 seconds
+		toggle_status_LEDs(PMS_LED,LOW,HIGH,5000);	// turn PMS status led on for 5 seconds
 	}
+	else
+	{
+		switch_status_LEDs_off(PMS_LED,HIGH);	// turn PMS status led off
+	}
+	
 
 	debug_outln_verbose(FPSTR(DBG_TXT_END_READING), FPSTR(SENSORS_PMSx003));
 	return s;
@@ -3733,7 +3738,7 @@ String fetchSensorGPSFromAtmega(){
 	}
 
 	obtain_sendTime();
-	toggle_status_LEDs(GPS_LED,LOW,HIGH,5000);	//	turn GPS status LED on for 5 seconds
+	toggle_status_LEDs(GPS_LED,LOW,HIGH,5000);	// turn GPS status LED on for 5 seconds
 
 	debug_outln_info(FPSTR(DBG_TXT_SEP));
 	return s;
@@ -3815,8 +3820,13 @@ void fetchSensorSPH0645(String& s){
 	  debug_outln_info(F("noise_Leq: "), String(value_SPH0645));
 	  add_Value2Json(s, F("noise_Leq"), String(value_SPH0645));
 	  obtain_sendTime();
-	  toggle_status_LEDs(MIC_LED,LOW,HIGH,5000);	//	turn mic status led on for 5 seconds
+	  toggle_status_LEDs(MIC_LED,LOW,HIGH,5000);	// turn mic status led on for 5 seconds
   }
+  else
+  {
+	  switch_status_LEDs_off(MIC_LED,HIGH);	// turn mic status led off
+  }
+  
 
 }
 
@@ -3833,11 +3843,11 @@ void init_RTC()
 		 rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 		// Set explicit time for example 19th May 2020 at 12 noon.
 		// rtc.adjust(DateTime(2020, 5, 19, 12, 00, 00));
-		switch_status_LEDs_off(RTC_LED,HIGH);	//	turn rtc status led off
+		switch_status_LEDs_off(RTC_LED,HIGH);	// turn rtc status led off
 	}
 	else
 	{
-		toggle_status_LEDs(RTC_LED,LOW,HIGH,5000);	//	turn RTC status led on for 5 seconds
+		toggle_status_LEDs(RTC_LED,LOW,HIGH,5000);	// turn RTC status led on for 5 seconds
 	}
 	
 }
@@ -4498,11 +4508,11 @@ static void powerOnTestSensors() {
 		delay(100);
 		debug_outln_info(F("Stopping PMS..."));
 		is_PMS_running = PMS_cmd(PmSensorCmd::Stop);
-		toggle_status_LEDs(PMS_LED,LOW,HIGH,2000);	//	turn PMS status led on for 2 seconds
+		toggle_status_LEDs(PMS_LED,LOW,HIGH,2000);	// turn PMS status led on for 2 seconds
 	}
 	else
 	{
-		switch_status_LEDs_off(PMS_LED,HIGH);	//	turn PMS status led off
+		switch_status_LEDs_off(PMS_LED,HIGH);	// turn PMS status led off
 	}
 
 	if (cfg::hpm_read) {
@@ -4523,22 +4533,22 @@ static void powerOnTestSensors() {
 	if (cfg::dht_read) {
 		dht.begin();										// Start DHT
 		debug_outln_info(F("Read DHT..."));
-		toggle_status_LEDs(DHT_LED,LOW,HIGH,2000);	//	turn DHT status led on for 2 seconds
+		toggle_status_LEDs(DHT_LED,LOW,HIGH,2000);	// turn DHT status led on for 2 seconds
 	}
 	else
 	{
-		switch_status_LEDs_off(DHT_LED,HIGH);	//	turn DHT status led off
+		switch_status_LEDs_off(DHT_LED,HIGH);	// turn DHT status led off
 	}
 
 	if (cfg::rtc_read) {
 		rtc.begin();
 		debug_outln_info(F("Read Time from RTC..."));
 		init_RTC();
-		toggle_status_LEDs(RTC_LED,LOW,HIGH,2000);	//	turn RTC status led on for 2 seconds
+		toggle_status_LEDs(RTC_LED,LOW,HIGH,2000);	// turn RTC status led on for 2 seconds
 	}
 	else
 	{
-		switch_status_LEDs_off(RTC_LED,HIGH);	//	turn RTC status led off
+		switch_status_LEDs_off(RTC_LED,HIGH);	// turn RTC status led off
 	}
 
 	if (cfg::htu21d_read) {
@@ -4588,27 +4598,27 @@ static void powerOnTestSensors() {
 	if(cfg::sph0645_read){
 		debug_outln_info(F("Read SPH0645..."));
 		init_SPH0645();
-		toggle_status_LEDs(MIC_LED,LOW,HIGH,2000);	//	turn mic status led on for 2 seconds
+		toggle_status_LEDs(MIC_LED,LOW,HIGH,2000);	// turn mic status led on for 2 seconds
 	}
 	else
 	{
-		switch_status_LEDs_off(MIC_LED,HIGH);	//	turn mic status led off
+		switch_status_LEDs_off(MIC_LED,HIGH);	// turn mic status led off
 	}
 
 	if (cfg::gps_read){
-		toggle_status_LEDs(GPS_LED,LOW,HIGH,2000);	//	turn GPS status led on for 2 seconds
+		toggle_status_LEDs(GPS_LED,LOW,HIGH,2000);	// turn GPS status led on for 2 seconds
 	}
 	else
 	{
-		switch_status_LEDs_off(GPS_LED,HIGH);	//	turn GPS status led off
+		switch_status_LEDs_off(GPS_LED,HIGH);	// turn GPS status led off
 	}
 
 	if (cfg::sd_read){
-		toggle_status_LEDs(LOGGER_LED,LOW,HIGH,2000);	//	turn logger status led on for 2 seconds
+		toggle_status_LEDs(LOGGER_LED,LOW,HIGH,2000);	// turn logger status led on for 2 seconds
 	}
 	else
 	{
-		switch_status_LEDs_off(LOGGER_LED,HIGH);	//	turn logger status led off
+		switch_status_LEDs_off(LOGGER_LED,HIGH);	// turn logger status led off
 	}
 
 }
